53610cb2b509b03e5c669a0e79c383b5d55508db
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-04-12 16:37:42 +0530

    modified the reduc logger for development mode only

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,14 +4,12 @@ import { logger } from "redux-logger";
 import { persistStore } from "redux-persist";
 
 //store expects an array of middelwares from redux
-const middelwares = [logger];
-console.log(process.env.NODE_ENV);
-const store = createStore(
-  rootReducer,
-  process.env.NODE_ENV == "development"
-    ? applyMiddleware(...middelwares)
-    : applyMiddleware([])
-);
+const middelwares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middelwares.push(logger);
+}
+const store = createStore(rootReducer, applyMiddleware(...middelwares));
 const persistor = persistStore(store);
 //creates a new provider that wraps our application
 export { store, persistor };
